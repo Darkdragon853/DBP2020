e270190694177d8c14510c8d5fca4e6dafdeb8c2
Lukas Hempel <lh11jise@studserv.uni-leipzig.de>
2020-07-01 23:08:01 +0200

    all necessary classes created

---
+++ Aufgabe3/src/main/java/PersonRelatedImpl.java
@@ -0,0 +1,31 @@
+public class PersonRelatedImpl implements PersonRelatedAPI {
+    @Override
+    public void getProfil(Long personID) {
+
+    }
+
+    @Override
+    public void getCommonInterestsOfMyFriends(Long personID) {
+
+    }
+
+    @Override
+    public void getCommonFriends(Long personID) {
+
+    }
+
+    @Override
+    public void getPersonsWithMostCommonInterests(Long personID) {
+
+    }
+
+    @Override
+    public void getJobRecommendation(Long personID) {
+
+    }
+
+    @Override
+    public void getShorthestFriendshipPath(Long personID) {
+
+    }
+}
